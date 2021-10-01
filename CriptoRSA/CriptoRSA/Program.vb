d16219f54a4e4c9de303e72956d2a98c01245fc8
Daniel A.B Vilela <dabvilela@gmail.com>
2021-10-01 15:02:13 -0300

    criptografia e descriptografia RSA finaliza

--- CriptoRSA/CriptoRSA/Program.vb
+++ CriptoRSA/CriptoRSA/Program.vb
@@ -1,39 +1,68 @@
 Imports System
+Imports System.Numerics
 
 Module Program
-    'numeros primos pré determinados coloque seus numeros primos
-    Property NumeroP As Integer = 17
-    Property NumeroQ As Integer = 41
-    'numero primo aleatório que seja menor que phin e maior que 1 e que seja primo entre si
-    Property e As Integer = 13
-    ' chave privada foi encontrada apartir do algoritmo d*e MOD phiN = 1
-    Property d As Integer = 197
-    Property MultplicacaoPrimos = NumeroP * NumeroQ
-    Property TotieneEulerFunction = (NumeroP - 1) * (NumeroQ - 1)
+
 
     Sub Main(args As String())
         Dim listaPalavra As New List(Of Char)
         Dim palavra = "daniel"
         palavra = palavra.ToLower
-        For Each letra In palavra
-            listaPalavra.Add(letra)
+
+        For Each letras In palavra
+            listaPalavra.Add(letras)
         Next
 
         Dim FraseCriptografadaRSA = CriptografarRSA(listaPalavra)
         Console.WriteLine($"A frase criptografada é igual a : {FraseCriptografadaRSA}")
+        Dim fraseDescriptografadaRSA = Descriptografar(FraseCriptografadaRSA)
+        Console.WriteLine(fraseDescriptografadaRSA)
 
     End Sub
 
+    Private Function Pow(numero As Integer, expoente As Integer) As BigInteger
+
+        Dim numeroElevado As BigInteger = 0
+        If expoente = 0 Then
+            Return 1
+        End If
+
+        numeroElevado = Pow(numero, expoente - 1) * numero
+
+        Return numeroElevado
+
+    End Function
+
     Private Function CriptografarRSA(listaPalavra As List(Of Char))
+        Dim e As Integer = 13
+        Dim Multiplicacaoprimos As Integer = 697
         Dim index = 0
         Dim criptografado As String = ""
 
         While index <= listaPalavra.Count - 1
-            criptografado += (((Asc(listaPalavra(index))) ^ e) Mod MultplicacaoPrimos).ToString
+            Dim numeroNaAsc = Asc(listaPalavra(index))
+
+            criptografado += ((Pow(numeroNaAsc, e)) Mod Multiplicacaoprimos).ToString
             index += 1
         End While
-
         Return criptografado
     End Function
+
+    Private Function Descriptografar(criptografado As String)
+        Dim e As Integer = 197
+        Dim Multiplicacaoprimos As Integer = 697
+        Dim index = 0
+        Dim indexfinal = index + 3
+        Dim descriptografado As String = ""
+
+        While index <= criptografado.Count - 1
+            Dim numeroLetra = criptografado.Substring(index, 3)
+            descriptografado += Chr(((Pow(numeroLetra, e)) Mod Multiplicacaoprimos))
+            index += 3
+        End While
+        Return descriptografado
+
+    End Function
+
 
 End Module
